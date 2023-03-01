2e808b24da3290d611148eb9578f0189b3976b4f
zhangyian <canlucingning@gmail.com>
2023-03-01 23:55:27 +0800

    策略者模式

---
+++ Day02/Day02/src/CashNormal.java
@@ -0,0 +1,8 @@
+public class CashNormal extends CashSuper {
+
+    //正常收费，原价返回
+    public double acceptCash(double price,int num){
+        return price * num;
+    }
+    
+}
---
+++ Day02/Day02/src/CashSuper.java
@@ -0,0 +1,6 @@
+
+public abstract class CashSuper {
+
+    public abstract double acceptCash(double price,int num);
+    
+}
